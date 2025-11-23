2415d90c35f3a9c4cd891847614d25e13310c7be
AkshayPatel-IIT-K <104145842+AkshayPatel-IIT-K@users.noreply.github.com>
2025-11-23 11:32:38 +0530

    New lessons and code

--- src/lessons/index.js
+++
@@ -1,9 +0,0 @@
-import L1 from "./L1_Greetings.json";
-import L2 from "./L2_Pronouns.json";
-import L3 from "./L3_SimpleSentences.json";
-
-export default {
-  L1,
-  L2,
-  L3,
-};
---
+++ src/src/lessons/index.js
@@ -0,0 +1,12 @@
+import L1 from "./L1_Greetings.json";
+import L2 from "./L2_BasicPhrases.json";
+import L3 from "./L3_Introductions.json";
+import L4 from "./L4_Numbers.json";
+import L5 from "./L5_Colors.json";
+import L6 from "./L6_Family.json";
+import L7 from "./L7_Animals.json";
+import L8 from "./L8_Food.json";
+import L9 from "./L9_Nature.json";
+import L10 from "./L10_Verbs.json";
+
+export default { L1, L2, L3, L4, L5, L6, L7, L8, L9, L10 };
